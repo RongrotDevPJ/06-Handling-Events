17d4175c0d6f87ed569c6803530ef901047e9c4d
Rongrot <rongrot_b@cmu.ac.th>
2025-10-12 14:27:37 +0700

    09 - React Hooks

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,35 +1,44 @@
 
-import { useState } from "react";
-import { AddForm } from "./Product";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Product from "./Product";
-import productsData from "../app/data";
+import { AddForm } from "./Product";
 
 let currentProductId = 9;
 
 function Home() {
-  const [products, setProducts] = useState(productsData || []);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const res = await axios.get(
+        "https://apimocha.com/react-redux-class/products"
+      );
+      setProducts(res.data);
+
+      
+      const maxId = res.data.reduce((m, p) => (p.id > m ? p.id : m), 0);
+      if (maxId > currentProductId) currentProductId = maxId;
+    }
+
+    getProducts();
+  }, []); 
 
   function addProduct(product) {
     const newProduct = { id: ++currentProductId, ...product };
-    setProducts([...products, newProduct]);
+    setProducts((prev) => [...prev, newProduct]);
   }
 
   return (
-    <>
+    <div className="container">
       <AddForm addProduct={addProduct} />
-      <hr />
-      <h2>Products</h2>
 
-      {products.length > 0 ? (
-        <ul className="Home__products">
-          {products.map((p) => (
-            <Product key={p.id} item={p} />
-          ))}
-        </ul>
-      ) : (
-        <div>Loading products....</div>
-      )}
-    </>
+      <ul className="Home__products">
+        {products.map((p) => (
+          <Product key={p.id} item={p} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
