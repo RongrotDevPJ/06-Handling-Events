44ceec362aa54a37c10062cd64fb92f781dcbe6d
Rongrot <rongrot_b@cmu.ac.th>
2025-10-12 15:21:09 +0700

    lab10Update

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,22 +1,26 @@
-  import React, { useState, useEffect } from "react";
-  import styled from "styled-components";
-  import axios from "axios";
-  import Product from "./Product";
-  import { AddForm } from "./Product";
-  import PropTypes from "prop-types";
-
- 
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import axios from "axios";
+import Product, { AddForm } from "./Product";
+const productsData = require("../app/data");
 
 let currentProductId = 9;
 
 function HomeBase({ className }) {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState(productsData);
 
   useEffect(() => {
     async function getProducts() {
       const res = await axios.get("https://apimocha.com/react-redux-class/products");
-      setProducts(res.data);
-      const maxId = res.data.reduce((m, p) => (p.id > m ? p.id : m), 0);
+      setProducts((prev) => {
+        const merged = [...prev, ...res.data];
+        const byId = new Map();
+        merged.forEach((p) => byId.set(p.id, p));
+        return Array.from(byId.values());
+      });
+      const all = [...productsData, ...res.data];
+      const maxId = all.reduce((m, p) => (p.id > m ? p.id : m), 0);
       if (maxId > currentProductId) currentProductId = maxId;
     }
     getProducts();
@@ -39,16 +43,12 @@ function HomeBase({ className }) {
   );
 }
 
-HomeBase.propTypes = {
-  className: PropTypes.string,
-};
-
+HomeBase.propTypes = { className: PropTypes.string };
 
 const Home = styled(HomeBase)`
   .Home__products {
     display: flex;
     flex-wrap: wrap;
-
     list-style-type: none;
     padding: 0;
     margin: 0 -12px;
