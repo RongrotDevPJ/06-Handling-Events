e54a6a34db0d3c0df384d05e5e3595abc698c504
Rongrot <rongrot_b@cmu.ac.th>
2025-10-18 11:31:11 +0700

    UpdateLab 11

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./features/Navbar.js";
-import Container from "./features/Container.js";
-import Home from "./features/Home.js";
-import AddForm from "./features/Product/AddForm.js";
-import UpdateForm from "./features/Product/UpdateForm.js"; 
+import Navbar from "./features/Navbar";
+import Container from "./features/Container";
+import Home from "./features/Home";
+import AddForm from "./features/Product/AddForm";
+import UpdateForm from "./features/Product/UpdateForm";
+import axios from "axios";
+const productsData = require("./app/data");
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getProducts() {
+      const res = await axios.get("https://apimocha.com/react-redux-class/products");
+      const merged = [...productsData, ...res.data];
+      const map = new Map(merged.map((p) => [p.id, p]));
+      setProducts(Array.from(map.values()));
+      setLoading(false);
+    }
+    getProducts();
+  }, []);
+
+  if (loading) return <div>Loading products…</div>;
+
   return (
     <>
       <Navbar />
       <Container>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home products={products} />} />
           <Route path="/create-product" element={<AddForm />} />
-          <Route path="/update-product/:id" element={<UpdateForm />} />
+          <Route path="/update-product/:id" element={<UpdateForm products={products} />} />
         </Routes>
       </Container>
     </>
--- src/features/Home.js
+++ src/features/Home.js
@@ -1,48 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import axios from "axios";
-import Product, { AddForm } from "./Product";
-const productsData = require("../app/data");
+import Product from "./Product";
 
-let currentProductId = 9;
-
-function HomeBase({ className }) {
-  const [products, setProducts] = useState(productsData);
-
-  useEffect(() => {
-    async function getProducts() {
-      const res = await axios.get("https://apimocha.com/react-redux-class/products");
-      setProducts((prev) => {
-        const merged = [...prev, ...res.data];
-        const byId = new Map();
-        merged.forEach((p) => byId.set(p.id, p));
-        return Array.from(byId.values());
-      });
-      const all = [...productsData, ...res.data];
-      const maxId = all.reduce((m, p) => (p.id > m ? p.id : m), 0);
-      if (maxId > currentProductId) currentProductId = maxId;
-    }
-    getProducts();
-  }, []);
-
-  function addProduct(product) {
-    const newProduct = { id: ++currentProductId, ...product };
-    setProducts((prev) => [...prev, newProduct]);
-  }
-
+function HomeBase({ className, products }) {
   return (
-     <div className={className}>
-    <ul className="Home__products">
-      {products.map((p) => (
-        <Product key={p.id} item={p} />
-      ))}
-    </ul>
-  </div>
+    <div className={className}>
+      <ul className="Home__products">
+        {products.map((p) => (
+          <Product key={p.id} item={p} />
+        ))}
+      </ul>
+    </div>
   );
 }
 
-HomeBase.propTypes = { className: PropTypes.string };
+HomeBase.propTypes = {
+  className: PropTypes.string,
+  products: PropTypes.array.isRequired,
+};
 
 const Home = styled(HomeBase)`
   .Home__products {
